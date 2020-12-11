d563f23dd81bae019f79ef65971f95ee890c2cac
RusTron <rusan.tron@gmail.com>
2020-12-11 02:18:07 +0200

    added readme

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { Header } from './components/Header';
-
-test('renders learn react link', () => {
-  render(<Header />);
-  const linkElement = screen.getByText(/products/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store';
 import { HashRouter } from 'react-router-dom';
+import store from './store';
 import App from './App';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
